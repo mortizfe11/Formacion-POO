4881a8ba40546d3ce6fb3f7fc20e38e0b686cec7
mortizfe11 <mortizfe11@gmail.com>
2022-10-19 13:24:29 +0200

    update ejercicio, con datos input

--- 2.Ejercicios/script.js
+++ 2.Ejercicios/script.js
@@ -1,11 +1,21 @@
-let x = 3, y = 10;
+//let x = 3, y = 10;
 //let txtdescrip = document.getElementsByClassName("descripcion")[0].textContent
-let firstdescrip = document.getElementsByClassName("descripcion")[0]
+var x = null;
+var y = null;
 
-firstdescrip.innerHTML += 
-`<p>Los valores son x: ${x}  y: ${y}</p>
-<p>Suma de x + y: ${x + y}</p>
-<p>Resta de x - y: ${x-y} </p>
-<p>Multiplicación de x * y: ${x*y} </p>
-<p>División de x / y:  ${x/y} </p>
-<p>Módulo de x % y:  ${x%y} </p>`
+function calcular(){
+
+    x = document.getElementById("x").valueAsNumber;
+    y = document.getElementById("y").valueAsNumber;
+    if (isNaN(x)) x = 3;
+    if (isNaN(y)) y = 10;
+    let firstdescrip = document.getElementsByClassName("descripcion")[0];
+
+    firstdescrip.innerHTML += 
+    `<p>Los valores son x: ${x}  y: ${y}</p>
+    <p>Suma: x + y = ${x+y}</p>
+    <p>Resta: x - y = ${x-y} </p>
+    <p>Multiplicación: x * y = ${x*y} </p>
+    <p>División: x / y = ${x/y} </p>
+    <p>Módulo: x % y = ${x%y} </p>`;
+}
