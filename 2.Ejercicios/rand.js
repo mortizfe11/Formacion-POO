e6c686bd401f50d38995698496971f62009bb141
Marta Ortiz <martaortiz@iMac02.local>
2022-11-10 09:31:53 +0100

    añadir pesos

--- 2.Ejercicios/rand.js
+++ 2.Ejercicios/rand.js
@@ -8,3 +8,36 @@ generarRand = (N, nMax = 53) => {
         return "El N es superior al nMax";
     }
 }
+
+promediarNotas = (arr, fixed = 2) => {
+    let count = 0;
+    arr.forEach(element => {
+        count+=element;
+    });
+    return (count/ arr.length).toFixed(fixed);
+}
+promediarNotasPesos = (arr, arrPesos, fixed = 2) => {
+    let count = 0, weight = 0;
+    if(arr.length == arrPesos.length){
+        for(let i = 0; i < arr.length; i++){
+            count+=arr[i]*arrPesos[i];
+            weight+=arrPesos[i];
+        };
+        return (count / weight).toFixed(fixed);
+    }else{
+        return "El tamaño de los arrays no es el mismo";
+    }
+}
+/*
+promediarNotasPesos = (arr, arrPesos, fixed = 2) => {
+    let count = 0, weight = 0;
+    if(arr.length == arrPesos.length){
+        arr.map((val, idx) => {
+            count+=val*arrPesos[idx];
+            weight+=arrPesos[idx];            
+        });
+        return (count / weight).toFixed(fixed);
+    }else{
+        return "El tamaño de los arrays no es el mismo";
+    }
+}*/
