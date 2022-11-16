63ba273c4157bf7d7da50311bc14273131f1e396
enrique ortiz <eortizto@gmail.com>
2022-11-16 15:16:36 +0100

    Tabla Students

---
+++ 7.BBDD/Students.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `w3schools-sql`.`Students` (
+  `StudentsId` INT NOT NULL AUTO_INCREMENT,
+  `Nombre` VARCHAR(50) NULL DEFAULT 'Alumno',
+  `Examen` DECIMAL(3,2) NULL DEFAULT 1,
+  PRIMARY KEY (`StudentsId`));
